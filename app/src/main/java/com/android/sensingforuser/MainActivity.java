329f52f59cc3f900e879bdc4657ee64254875c71
Yunho Kim <31121701+Kandreaop@users.noreply.github.com>
2022-05-16 14:48:00 +0900

    MainActivity UI수정

--- app/src/main/java/com/android/sensingforuser/MainActivity.java
+++ app/src/main/java/com/android/sensingforuser/MainActivity.java
@@ -1,10 +1,13 @@
 package com.android.sensingforuser;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
@@ -13,27 +16,30 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
 
-    private Button sensingBtn;
-    private TextView textView;
+    private ImageView sensingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        sensingBtn = (Button) findViewById(R.id.sensingBtn);
-        textView = (TextView) findViewById(R.id.textView);
-        textView.setText("내 위치: ");
+        sensingBtn = (ImageView) findViewById(R.id.sensingButton);
     }
 
 
+
+
     //센싱 버튼 클릭
-    public void mOnSensing(View v){
+    public void mOnSensing(View view){
         //예시. 버튼 눌렀을 때 TextView 바꾸기
-        textView.setText("내 위치: " + " blabla...");
-
-
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("위치").setMessage("당신은 ~에 있습니다");
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
         //TODO: 센싱 후 textView를 위치 측정한 장소로 바꾸기.
     }
 }
